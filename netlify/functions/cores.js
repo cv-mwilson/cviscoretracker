570a8afb5fdf04c4a31c81c3d699e2b41bda70c8
cv-mwilson <mwilson@cardinalvalley.com>
2026-04-17 12:48:09 -0400

    Create cores.js

---
+++ netlify/functions/cores.js
@@ -0,0 +1,27 @@
+/**
+ * GET /api/cores
+ * Fetches outstanding core charge lines from NetSuite
+ */
+
+const { callRESTlet } = require('./netsuite');
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'GET') {
+    return { statusCode: 405, body: JSON.stringify({ error: 'Method not allowed' }) };
+  }
+
+  try {
+    const data = await callRESTlet('GET', {});
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(data)
+    };
+  } catch (e) {
+    console.error('GET cores error:', e.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false, error: e.message })
+    };
+  }
+};
